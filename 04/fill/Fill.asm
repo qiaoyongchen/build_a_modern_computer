8bb42e9eb884fd768c1ffbae080236414a2ef3dc
bojimiya <qiaoyongchen@hotmail.com>
2020-04-17 09:08:38 +0800

    x

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    @24575
+    D = A
+    // R0存储屏幕最大地址
+    @0
+    M = D
+    // R1存储屏幕当前地址
+    @SCREEN
+    D = A
+    @1
+    M = D
+(LOOP)
+    @KBD
+    D = M
+    @FILL
+    D;JGT
+    @CLEAR
+    0;JMP
+(FILL)
+    // 判断屏幕是否为满
+    @0
+    D = M
+    @1
+    D = D - M
+    @LOOP
+    D;JLT
+    @1
+    // 缓存当前地址
+    D = M
+    // 将当前地址推入A
+    A = M
+    // 将地址对应的屏幕位置变黑
+    M = -1
+    // 当前地址+1
+    @1
+    M = D + 1
+    @LOOP
+    0;JMP
+(CLEAR)
+    // 判断屏幕是否为空
+    @SCREEN
+    D = A
+    @1
+    D = D - M
+    @LOOP
+    D;JGT
+    @1
+    // 缓存当前地址
+    D = M
+    // 将当前地址推入A
+    A = M
+    // 将地址对应的屏幕位置变白
+    M = 0
+    // 当前地址-1
+    @1
+    M = D - 1
+    @LOOP
+    0;JMP
