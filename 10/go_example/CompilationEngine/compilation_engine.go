45ec2f21003c8b27527f40e69d196ac93ac59480
bojimiya <qiaoyongchen@hotmail.com>
2020-07-05 23:23:08 +0800

    jack 编译器阶段性提交

--- 10/go_example/CompilationEngine/compilation_engine.go
+++ 10/go_example/CompilationEngine/compilation_engine.go
@@ -20,7 +20,6 @@ func NewCompilationEngine(jt *jt.JackTokenizer, fo *os.File) *CompilationEngine 
 
 // CompileClass 编译整个类
 func (p *CompilationEngine) CompileClass() {
-	outputstring := ""
 	p.jt.Advance()
 	if !p.jt.HasMoreTokens() {
 		panic("c_001")
@@ -33,7 +32,8 @@ func (p *CompilationEngine) CompileClass() {
 	if smb != JackTokenizer.KEY_CLASS {
 		panic("c_003")
 	}
-	outputstring += "<class>"
+	p.o.WriteString("<class>")
+
 	p.jt.Advance()
 	if !p.jt.HasMoreTokens() {
 		panic("c_004")
@@ -42,7 +42,8 @@ func (p *CompilationEngine) CompileClass() {
 	if tknType2 != JackTokenizer.TKN_IDENTIFIER {
 		panic("c_005")
 	}
-	outputstring += fmt.Sprintf("<identifier>%s</identifier>", p.jt.Identifierr())
+	p.o.WriteString(fmt.Sprintf("<identifier>%s</identifier>", p.jt.Identifierr()))
+
 	p.jt.Advance()
 	if !p.jt.HasMoreTokens() {
 		panic("c_006")
@@ -54,16 +55,44 @@ func (p *CompilationEngine) CompileClass() {
 	if p.jt.Symbol() != JackTokenizer.SYM_LEFT_PARENTHESIS {
 		panic("c_008")
 	}
-	outputstring += fmt.Sprintf("<symbol>%s</symbol>", p.jt.Symbol())
+	p.o.WriteString(fmt.Sprintf("<symbol>%s</symbol>", p.jt.Symbol()))
 
-	// TODO
+	p.jt.Advance()
+	if !p.jt.HasMoreTokens() {
+		panic("c_009")
+	}
+	tknType4 := p.jt.TokenType()
 
-	outputstring += "</class>"
+	// 类下面一层似乎之需要定义类静态声明/字段声明 或 定义方法
+	// 其他都是嵌套在其他结构吧，后面碰到再说吧
+	// 直到遇到他命中注定的右括号才算完美结束, 其他的和咱都不搭....
+	for {
+		if tknType4 == JackTokenizer.TKN_KEYWORD {
+			switch p.jt.Keyword() {
+			case JackTokenizer.KEY_FIELD, JackTokenizer.KEY_STATIC:
+				p.CompileClassVarDec()
+			case JackTokenizer.KEY_CONSTRUCTOR, JackTokenizer.KEY_FUNCTION, JackTokenizer.KEY_METHOD:
+				p.CompileSubroutine()
+			default:
+				panic("c_011")
+			}
+			p.jt.Advance()
+			tknType4 = p.jt.TokenType()
+		} else if tknType4 == JackTokenizer.TKN_SYMBOL && p.jt.Symbol() == JackTokenizer.SYM_RIGHT_PARENTHESIS {
+			p.o.WriteString(fmt.Sprintf("<symbol>%s</symbol>", p.jt.Symbol()))
+			break
+		} else {
+			panic("c_012")
+		}
+	}
+
+	// 类终结符
+	p.o.WriteString("</class>")
 }
 
 // CompileClassVarDec 编译类静态声明或字段声明
 func (p *CompilationEngine) CompileClassVarDec() {
-
+	// TODO
 }
 
 // CompileSubroutine 编译方法，函数或构造函数
